13df2ca68effe2289a6ff4d8fedb96a6d4452ee5
yafimahardika <yafimahardika16@gmail.com>
2026-04-20 00:41:33 +0700

    menambahkan produk hardisk ke katalog

--- app/Http/Controllers/KatalogController.php
+++ app/Http/Controllers/KatalogController.php
@@ -13,12 +13,13 @@ class KatalogController extends Controller
             ["id"=>2,"nama"=>"Mouse","harga"=>150000],
             ["id"=>3,"nama"=>"Keyboard","harga"=>350000],
             ["id"=>4,"nama"=>"Monitor","harga"=>2000000],
-            ["id"=>5,"nama"=>"Flashdisk","harga"=>100000]
+            ["id"=>5,"nama"=>"Flashdisk","harga"=>100000],
+            ["id"=>6,"nama"=>"Harddisk", "harga"=>500000]
         ];
 
         return view('katalog.index',
         [
-        'produk' =>$produk
+        'produk' =>$produk // 'produk' adalah isi paket dan $produk adalah isi paket
         ]);
     }
 
@@ -29,7 +30,8 @@ class KatalogController extends Controller
         2 => ["nama"=>"Mouse","harga"=>150000],
         3 => ["nama"=>"Keyboard","harga"=>350000],
         4 => ["nama"=>"Monitor","harga"=>2000000],
-        5 => ["nama"=>"Flashdisk","harga"=>100000]
+        5 => ["nama"=>"Flashdisk","harga"=>100000],
+        6 => ["nama"=>"Hardisk", "harga"=>500000]
     ];
 
     if (!isset($produk[$id])) {
