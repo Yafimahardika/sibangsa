eabc5e4c68aa2a9aa72408be67c1004833a26130
yafimahardika <yafimahardika16@gmail.com>
2026-06-26 15:01:37 +0700

    Menambahkan logo pada login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 btn bg-green-600">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
